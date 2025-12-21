e2f5563972665b1ee9999b89a4e73f018f6589b4
CarlosJ <carlos@MacBook-Pro-de-Carlos-2.local>
2025-12-21 17:20:50 +0100

    Change string

--- src/main.py
+++ src/main.py
@@ -76,7 +76,7 @@ async def health_check() -> dict:
     logger.debug("Health check endpoint called")
     return {
         "status": "healthy",
-        "message": f"{settings.APP_NAME} is running successfully 1"
+        "message": f"{settings.APP_NAME} is running successfully 2."
     }
 
 
